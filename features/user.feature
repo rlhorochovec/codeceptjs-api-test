04dd2d282d2090896707e5db0f095509ad3a35cc
Rafael Horochovec <rl.horochovec@gmail.com>
2024-02-10 11:02:53 -0300

    Adicionando cenários

--- features/user.feature
+++ features/user.feature
@@ -1,7 +1,28 @@
-Feature: Usuários
+Feature: API de Usuários
+
+Background:
+    Given que possuo acesso a API de usuários
 
 @POST
-Scenario: Criar usuário
-    Given que estou cadastrando novo usuário
-    When enviar os dados do usuário
-    Then apresentará sucesso no cadastro
+Scenario: Criar um novo usuário
+    When eu enviar uma solicitação para criar um novo usuário
+    Then o novo usuário deve ser criado com sucesso
+    And eu devo receber um código de status HTTP 201
+
+@PUT
+Scenario: Atualizar informações de um usuário 
+    When eu enviar uma solicitação para atualizar os detalhes do usuário com ID 4
+    Then os detalhes do usuário devem ser atualizados com sucesso
+    And eu devo receber um código de status HTTP 200
+
+@GET
+Scenario: Obter detalhes de um usuário existente
+    When eu solicitar os detalhes do usuário com ID 6
+    Then devo receber os detalhes do usuário corretos
+    And eu devo receber um código de status HTTP 200
+
+@DEL
+Scenario: Remover um usuário existente
+    When eu enviar uma solicitação para remover o usuário com ID 5
+    Then o usuário deve ser removido com sucesso
+    And eu devo receber um código de status HTTP 204
--- step_definitions/user_steps.js
+++ step_definitions/user_steps.js
@@ -1,23 +1,64 @@
 const { I } = inject();
 
-Given('que estou cadastrando novo usuário', () => {
+Given('que possuo acesso a API de usuários', () => {});
+
+When('eu enviar uma solicitação para criar um novo usuário', () => {
   body = {
     name: 'hulk',
     job: 'hero'
   }
+  I.sendPostRequest('/users', body)
 });
 
-When('enviar os dados do usuário', () => {
-  I.sendPostRequest('/users', body);
-});
-
-Then('apresentará sucesso no cadastro', () => {
-  I.seeResponseCodeIsSuccessful();
-  I.seeResponseCodeIs(201);
+Then('o novo usuário deve ser criado com sucesso', () => {
+  I.seeResponseCodeIsSuccessful()
   I.seeResponseContainsJson(
     {
       name: 'hulk',
       job: 'hero',
     }
   );
+});
+
+Then('eu devo receber um código de status HTTP {int}', (num) => {
+  I.seeResponseCodeIs(num)
+});
+
+When('eu solicitar os detalhes do usuário com ID {int}', (num) => {
+  I.sendGetRequest('/users/' + num)
+});
+
+Then('devo receber os detalhes do usuário corretos', () => {
+  I.seeResponseCodeIsSuccessful()
+  I.seeResponseContainsJson(
+    {
+      "data": {
+        "id": 6,
+        "email": "tracey.ramos@reqres.in",
+        "first_name": "Tracey",
+        "last_name": "Ramos",
+        "avatar": "https://reqres.in/img/faces/6-image.jpg"
+      }
+    }
+  );
+});
+
+When('eu enviar uma solicitação para remover o usuário com ID {int}', (num) => {
+  I.sendDeleteRequest('/users/' + num)
+});
+
+Then('o usuário deve ser removido com sucesso', () => {
+  I.seeResponseCodeIsSuccessful()
+});
+
+When('eu enviar uma solicitação para atualizar os detalhes do usuário com ID {int}', (num) => {
+  body = {
+    name: 'cleiton',
+    job: 'programador'
+  }
+  I.sendPutRequest('/users/' + num)
+});
+
+Then('os detalhes do usuário devem ser atualizados com sucesso', () => {
+  I.seeResponseCodeIsSuccessful()
 });
